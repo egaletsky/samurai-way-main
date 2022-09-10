8ad0ffb55b57b36e94fb34686443e3f7026a933c
egaletsky <galetsky@gmail.com>
2022-09-10 07:54:19 +0700

    1-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
+
+function hello() {
+    //debugger
+    alert('Hello !!')
+}
+
+hello()
+
 function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!!!
+        <Rating/>
     </div>
   );
 }
 
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>
+            <div>*Star*</div>
+        </div>
+    );
+}
 export default App;
